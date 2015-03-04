5230f4e3512207c95cab3b7fcd9b5683ff6fc17e
caramdache <caramdache@users.noreply.github.com>
2015-03-04 17:01:25 +0100

    Update UIWindow+extensions.rb

--- Classes/UIWindow+extensions.rb
+++ Classes/UIWindow+extensions.rb
@@ -1,29 +1,29 @@
 class UIWindow
-	def swizzlingMessage
-		"Method Swizzlings: sendEvent: and description"
-	end
+  def swizzlingMessage
+    "Method Swizzlings: sendEvent: and description"
+  end
 
-	def swizzle
-		range = self.description.rangeOfString(self.swizzlingMessage, options:NSStringCompareOptions.LiteralSearch, range:nil, locale:nil)
-		if range and range.startIndex != nil then
-			return
-		end
-		
-		sendEvent = class_getInstanceMethod(object_getClass(self), "sendEvent:")
-		swizzledSendEvent = class_getInstanceMethod(object_getClass(self), "swizzledSendEvent:")
-		method_exchangeImplementations(sendEvent, swizzledSendEvent)
+  def swizzle
+    range = self.description.rangeOfString(self.swizzlingMessage, options:NSStringCompareOptions.LiteralSearch, range:nil, locale:nil)
+    if range and range.startIndex != nil then
+      return
+    end
+    
+    sendEvent = class_getInstanceMethod(object_getClass(self), "sendEvent:")
+    swizzledSendEvent = class_getInstanceMethod(object_getClass(self), "swizzledSendEvent:")
+    method_exchangeImplementations(sendEvent, swizzledSendEvent)
 
-		description = class_getInstanceMethod(object_getClass(self), "description")
-		swizzledDescription = class_getInstanceMethod(object_getClass(self), "swizzledDescription")
-		method_exchangeImplementations(description, swizzledDescription)
-	end
+    description = class_getInstanceMethod(object_getClass(self), "description")
+    swizzledDescription = class_getInstanceMethod(object_getClass(self), "swizzledDescription")
+    method_exchangeImplementations(description, swizzledDescription)
+  end
 
-	def swizzledSendEvent(event)
-		MZRPresentationView.sharedInstance.handleEvent(event)
-		self.swizzledSendEvent(event)
-	end
+  def swizzledSendEvent(event)
+    MZRPresentationView.sharedInstance.handleEvent(event)
+    self.swizzledSendEvent(event)
+  end
 
-	def swizzledDescription
-		self.swizzledDescription + "; " + self.swizzlingMessage
-	end
+  def swizzledDescription
+    self.swizzledDescription + "; " + self.swizzlingMessage
+  end
 end
