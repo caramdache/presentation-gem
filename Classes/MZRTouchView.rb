f3931608c2a106b52ced609fda846d7c10041903
caramdache <caramdache@users.noreply.github.com>
2015-03-04 17:02:11 +0100

    Update MZRTouchView.rb

--- Classes/MZRTouchView.rb
+++ Classes/MZRTouchView.rb
@@ -2,21 +2,21 @@ class MZRTouchView < UIImageView
   weak_attr_accessor :touch
   
   def defaultTintColor
-  	@color ||= UIColor.colorWithRed(52/255.0, green:152/255.0, blue:219/255.0, alpha:0.8)
+    @color ||= UIColor.colorWithRed(52/255.0, green:152/255.0, blue:219/255.0, alpha:0.8)
   end
 
   def defaultImage
-  	@image ||= begin
-	    rect = CGRectMake(0, 0, 60.0, 60.0)
-	    UIGraphicsBeginImageContextWithOptions(rect.size, false, 0)
-	    contextRef = UIGraphicsGetCurrentContext()
-	    CGContextSetFillColorWithColor(contextRef, UIColor.blackColor.CGColor)
-	    CGContextFillEllipseInRect(contextRef, rect)
-	    image = UIGraphicsGetImageFromCurrentImageContext()
-	    UIGraphicsEndImageContext()
-	    image.imageWithRenderingMode(UIImageRenderingModeAlwaysTemplate)
-	    image
-	  end
+    @image ||= begin
+      rect = CGRectMake(0, 0, 60.0, 60.0)
+      UIGraphicsBeginImageContextWithOptions(rect.size, false, 0)
+      contextRef = UIGraphicsGetCurrentContext()
+      CGContextSetFillColorWithColor(contextRef, UIColor.blackColor.CGColor)
+      CGContextFillEllipseInRect(contextRef, rect)
+      image = UIGraphicsGetImageFromCurrentImageContext()
+      UIGraphicsEndImageContext()
+      image.imageWithRenderingMode(UIImageRenderingModeAlwaysTemplate)
+      image
+    end
   end
   
   def initWithImage(image, color:color)
